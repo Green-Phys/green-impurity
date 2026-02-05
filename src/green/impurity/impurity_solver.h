4f650f9de9a119c63aa993d5d098ccad5ffff2e7
Gaurav Harsha <gauravharsha05@gmail.com>
2026-02-05 13:18:52 -0500

    fix typo in output mesage

--- src/green/impurity/impurity_solver.h
+++ src/green/impurity/impurity_solver.h
@@ -425,7 +425,7 @@ namespace green::impurity {
           matrix(sigma_w_loc_new(it, is)) += matrix(uu).transpose() * matrix(sigma_as(it, is)) * matrix(uu);
         }
       }
-      std::cout << "Imputity " << imp << " finished" << std::endl;
+      std::cout << "Impurity " << imp << " finished" << std::endl;
     }
     return std::make_tuple(sigma_inf_loc_new, sigma_w_loc_new);
   }
