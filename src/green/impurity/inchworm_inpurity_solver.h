8b5d9b869678c6fa55b634647486d4914bfb2893
Gaurav Harsha <gauravharsha05@gmail.com>
2026-02-05 19:59:45 -0500

    add support for all 2e notations

--- src/green/impurity/inchworm_inpurity_solver.h
+++ src/green/impurity/inchworm_inpurity_solver.h
@@ -121,11 +121,21 @@ namespace green::impurity {
           }
         }
         std::cout << "------------------------------------------------------------" << std::endl;
-        std::cout << "NOTE: Will write interaction tensor in the Tensor notation!" << std::endl;
-        std::cout << "i.,e., 2-body term = (1/2) * U(ijkl) cdag_i cdag_k c_k c_l" << std::endl;
+        std::cout << "NOTE: Writing interaction tensor in the all three notations!" << std::endl;
+        std::cout << "1. Chemist notation (imp_" + std::to_string(imp_n) + "_Uijkl_chem.txt): " << std::endl;
+        std::cout << "\t 2-body term = (1/2) * U(ijkl) cdag_i cdag_k c_l c_j" << std::endl;
+        std::cout << "2. Physicist notation (imp_" + std::to_string(imp_n) + "_Uijkl_phys.txt): " << std::endl;
+        std::cout << "\t 2-body term = (1/2) * U(ijkl) cdag_i cdag_j c_l c_k" << std::endl;
+        std::cout << "3. Tensor notation (imp_" + std::to_string(imp_n) + "_Uijkl_cthyb.txt): " << std::endl;
+        std::cout << "\t 2-body term = (1/2) * U(ijkl) cdag_i cdag_j c_k c_l" << std::endl;
+        std::cout << "Use the appropriate file in your workflow." << std::endl;
         std::cout << "------------------------------------------------------------" << std::endl;
-        std::ofstream U_file("imp_" + std::to_string(imp_n) + "_Uijkl.txt");
-        U_file << non_zero << "\n";
+        std::ofstream U_file_cthyb("imp_" + std::to_string(imp_n) + "_Uijkl_cthyb.txt");
+        std::ofstream U_file_phys("imp_" + std::to_string(imp_n) + "_Uijkl_phys.txt");
+        std::ofstream U_file_chem("imp_" + std::to_string(imp_n) + "_Uijkl_chem.txt");
+        U_file_cthyb << non_zero << "\n";
+        U_file_phys << non_zero << "\n";
+        U_file_chem << non_zero << "\n";
         size_t idx = 0;
         for (size_t i = 0; i < nio * ns; ++i) {
           for (size_t j = 0; j < nio * ns; ++j) {
@@ -137,9 +147,19 @@ namespace green::impurity {
                 size_t L = l / ns;
                 // Internally, IJKL and ijkl will be treated as chemist notation indices, i.e.
                 //    i = (I,s1); j = (J,s1); k = (K,s2); l = (L,s2)
-                // When we store to Uijkl.txt, we will re-arrange the indices to match Tensor notation.
+                // When we store to Uijkl.txt, we will re-arrange the indices to match each of the three formats
+
                 // --------------------------------------------------------------
-                // Logic for storing in Tensor notation
+                // 1. Logic for storing in Physicist notation
+                // --------------------------------------------------------------
+                // We have the tensor notation:    V(ijkl) cdag_i cdag_j c_l c_k
+                // Let's rearrange by changing dummy index labels:
+                //     V(ikjl) cdag_i cdag_k c_l c_j
+                // Compare with:             U(ijkl) cdag_i cdag_k c_l c_j
+                // Therefore,   V(ikjl) = U(ijkl)
+
+                // --------------------------------------------------------------
+                // 2. Logic for storing in Tensor notation
                 // --------------------------------------------------------------
                 // We have the tensor notation:    V(ijkl) cdag_i cdag_j c_k c_l
                 // Let's rearrange by changing dummy index labels:
@@ -148,7 +168,9 @@ namespace green::impurity {
                 // Therefore,   V(iklj) = U(ijkl)
                 if (std::abs(interaction(I, J, K, L)) < 1e-10) continue;
                 if (rhf) {
-                  U_file << idx << "\t" << i << " " << k << " " << l << " " << j << " " << interaction(I, J, K, L) << " " << 0.0 << "\n";
+                  U_file_chem << idx << "\t" << i << " " << j << " " << k << " " << l << " " << interaction(I, J, K, L) << " " << 0.0 << "\n";
+                  U_file_phys << idx << "\t" << i << " " << k << " " << j << " " << l << " " << interaction(I, J, K, L) << " " << 0.0 << "\n";
+                  U_file_cthyb << idx << "\t" << i << " " << k << " " << l << " " << j << " " << interaction(I, J, K, L) << " " << 0.0 << "\n";
                   ++idx;
                 } else {
                   // Deal with spin only for UHF
@@ -159,14 +181,18 @@ namespace green::impurity {
                   if (i == k || j == l) continue;       // ignore double creation/annihilation in same spin-orbitals (different from I,J,K,L which are atomic orbitals)
                   if (s1 != s2 || s3 != s4) continue;   // ignore spin-flip terms
                   // what remains is a valid term in Uijkl
-                  U_file << idx << "\t" << i << " " << k << " " << l << " " << j << " " << interaction(I, J, K, L) << " " << 0.0 << "\n";
+                  U_file_chem << idx << "\t" << i << " " << j << " " << k << " " << l << " " << interaction(I, J, K, L) << " " << 0.0 << "\n";
+                  U_file_phys << idx << "\t" << i << " " << k << " " << j << " " << l << " " << interaction(I, J, K, L) << " " << 0.0 << "\n";
+                  U_file_cthyb << idx << "\t" << i << " " << k << " " << l << " " << j << " " << interaction(I, J, K, L) << " " << 0.0 << "\n";
                   ++idx;
                 }
               }
             }
           }
         }
-        U_file.close();
+        U_file_cthyb.close();
+        U_file_chem.close();
+        U_file_phys.close();
       }
       // Inchworm requires another SLURM job, so we will return ZERO here, and update the actual
       // result after inchworm calculation is done.
